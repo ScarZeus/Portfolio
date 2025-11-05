1a7e62b5dce16ada205e4b77089859b6469ffd04
K Kevin Gladson <kevingladson2427@gmail.com>
2025-11-05 20:45:09 +0530

    header component has been initialized

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './portfolio-component/portfolio-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PortfolioComponent },
+  { path: '**', redirectTo: '' } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled' })],
+  exports: [RouterModule]
+})
---
+++ frontend/src/app/portfolio-component/portfolio-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+
+@Component({
+  selector: 'app-portfolio-component',
+  imports: [Header],
+  templateUrl: './portfolio-component.html',
+  styleUrl: './portfolio-component.css',
+})
+export class PortfolioComponent {
+
+}
