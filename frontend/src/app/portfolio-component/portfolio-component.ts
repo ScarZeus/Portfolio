1dff63c7274b90ef500629b0eedc4d88fa48f9b3
K Kevin Gladson <kevingladson2427@gmail.com>
2025-11-06 12:19:44 +0530

    hero section setup

--- frontend/src/app/portfolio-component/portfolio-component.ts
+++ frontend/src/app/portfolio-component/portfolio-component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
+import { HeroSection } from '../hero-section/hero-section';
 
 @Component({
   selector: 'app-portfolio-component',
-  imports: [Header],
+  imports: [Header,HeroSection],
   templateUrl: './portfolio-component.html',
   styleUrl: './portfolio-component.css',
 })
