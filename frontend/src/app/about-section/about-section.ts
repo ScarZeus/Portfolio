f5f917f044c1b8093e0c6d97f0b5861e5fc14175
K Kevin Gladson <kevingladson2427@gmail.com>
2025-11-07 21:01:04 +0530

    about section setup is complete

--- frontend/src/app/about-section/about-section.ts
+++ frontend/src/app/about-section/about-section.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './about-section.css',
 })
 export class AboutSection {
-
+    aboutDescription: string = "I like turning ideas into things that actually work."+
+     "When I'm building something, I care about clarity — in code, in design, and in how users experience it."+
+     " I don’t chase trends or stack logos; I focus on solving the problem in front of me with the most straightforward, "+
+     "maintainable approach. Every project teaches me something, "+
+     "and I enjoy the process of improving the system a little more than the day before.";
 }
--- frontend/src/app/portfolio-component/portfolio-component.ts
+++ frontend/src/app/portfolio-component/portfolio-component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { HeroSection } from '../hero-section/hero-section';
+import { AboutSection } from '../about-section/about-section';
 
 @Component({
   selector: 'app-portfolio-component',
-  imports: [Header,HeroSection],
+  imports: [Header,HeroSection,AboutSection],
   templateUrl: './portfolio-component.html',
   styleUrl: './portfolio-component.css',
 })
