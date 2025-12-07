18c306d750bc9308427369a68e4f024f13af0140
K Kevin Gladson <kevingladson2427@gmail.com>
2025-12-07 18:50:23 +0530

    cross side scripting error

--- frontend/src/app/projects-section/projects-section.ts
+++ frontend/src/app/projects-section/projects-section.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProjectsModel } from '../models/projects-model';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-projects-section',
@@ -9,13 +10,31 @@ import { ProjectsModel } from '../models/projects-model';
   styleUrl: './projects-section.css',
 })
 export class ProjectsSection {
-    projectsList: ProjectsModel[] = [
-        {
-          title: 'Portfolio Website',
-          description: 'A personal portfolio website showcasing my skills and projects.',
-          imageRef: 'assets/images/portfolio.png',
-          githubLink: '',
-          id: 0
-        }
-    ];
+  
+  projectsListSanitized: {
+    id: number;
+    imageRef: SafeResourceUrl;
+    title: string;
+    description: string;
+    githubLink: string;
+  }[] = [];
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  projectsList: ProjectsModel[] = [
+    {
+      title: 'Portfolio Website',
+      description: 'A personal portfolio website showcasing my skills and projects.',
+      imageRef: 'https://drive.google.com/uc?export=view&id=1LWLb0beFwNK3vqxEoCsVK3NlnnAROA3h',
+      githubLink: '',
+      id: 0
+    }
+  ];
+
+  ngOnInit(): void {
+    this.projectsListSanitized = this.projectsList.map(project => ({
+      ...project,
+      imageRef: this.sanitizer.bypassSecurityTrustResourceUrl(project.imageRef)
+    }));
+  }
 }
