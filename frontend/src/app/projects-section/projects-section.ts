84cbd5f606650908a2f3a7f9c76829907e98a9e5
K Kevin Gladson <kevingladson2427@gmail.com>
2025-12-07 18:51:42 +0530

    google drive attempt failed

--- frontend/src/app/projects-section/projects-section.ts
+++ frontend/src/app/projects-section/projects-section.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProjectsModel } from '../models/projects-model';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-projects-section',
@@ -10,31 +9,13 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrl: './projects-section.css',
 })
 export class ProjectsSection {
-  
-  projectsListSanitized: {
-    id: number;
-    imageRef: SafeResourceUrl;
-    title: string;
-    description: string;
-    githubLink: string;
-  }[] = [];
-
-  constructor(private sanitizer: DomSanitizer) {}
-
-  projectsList: ProjectsModel[] = [
-    {
-      title: 'Portfolio Website',
-      description: 'A personal portfolio website showcasing my skills and projects.',
-      imageRef: 'https://drive.google.com/uc?export=view&id=1LWLb0beFwNK3vqxEoCsVK3NlnnAROA3h',
-      githubLink: '',
-      id: 0
-    }
-  ];
-
-  ngOnInit(): void {
-    this.projectsListSanitized = this.projectsList.map(project => ({
-      ...project,
-      imageRef: this.sanitizer.bypassSecurityTrustResourceUrl(project.imageRef)
-    }));
-  }
+    projectsList: ProjectsModel[] = [
+        {
+          title: 'Portfolio Website',
+          description: 'A personal portfolio website showcasing my skills and projects.',
+          imageRef: 'assets/images/portfolio.png',
+          githubLink: '',
+          id: 0
+        }
+    ];
 }
